14ffdb7beb63c276796daa61705fa505ae0d34ea
Rafa D. Latorre López Villalta <sauco82@gmail.com>
2022-04-13 17:06:16 +0200

    Responsive Refactor intro explanations

--- components/intro/Intro.jsx
+++ components/intro/Intro.jsx
@@ -1,8 +1,13 @@
-import Layout from "../Layout/layout"
 import { useState } from "react";
 
-
-import { portfolio_article__intro } from "./Intro.module.scss"
+import Layout from "../Layout/layout"
+import { 
+  intro,
+  content,
+  gallery,
+  copy,
+  navigator,
+} from "./Intro.module.scss"
 
 export default function Intro({children, images}) {
     const [currentImage, setCurrentImage] = useState(0),
@@ -20,21 +25,21 @@ export default function Intro({children, images}) {
     };
 
     return (
-      <div className={portfolio_article__intro}>
+      <div className={intro}>
         <Layout>
-          <div className="grid">
-            <div className="g-col-8 justify-self-end align-self-center mr-3">
+          <div className={content}>
+            <div className={gallery}>
               <img src={images[currentImage]} />
             </div>
-            <div className="g-col-4 align-self-center ">
-              {children}
-              {images.length > 1 &&
-                <div className="mt-4">
-                  <a href="#" className={`btn mr-3 ${prevDisabled && "disabled"}`} onClick={setPrev}> &lt; Prev</a>
-                  <a href="#" className={`btn ${nextDisabled && "disabled"}`} onClick={setNext}>Next &gt; </a>
-                </div>
-              }
+            <div className={copy}>
+              {children}              
             </div>
+            {images.length > 1 &&
+              <div className={navigator}>
+                <a href="#" className={`btn mr-3 ${prevDisabled && "disabled"}`} onClick={setPrev}> &lt; Prev</a>
+                <a href="#" className={`btn ${nextDisabled && "disabled"}`} onClick={setNext}>Next &gt; </a>
+              </div>
+            }
           </div>
         </Layout>
       </div>
