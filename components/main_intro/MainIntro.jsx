f875fc3d7257884b2e9817a6d0a57eb5dc3320e9
Rafa D. Latorre López Villalta <sauco82@gmail.com>
2022-05-20 11:57:05 +0200

    Improved intro styling

--- components/main_intro/MainIntro.jsx
+++ components/main_intro/MainIntro.jsx
@@ -1,16 +1,23 @@
-import { intro, intro_container } from "./main-intro.module.scss"
+import { intro, intro_container, me, hi } from "./main-intro.module.scss"
 
 export default function MainIntro(){
   return (
     <div className={intro_container}>
-      <h1 className={intro}>
-        Hi! I'm <strong className='text-color-purple'>Rafa</strong> and I have spent the last
-        <strong className='text-color-soft-red inline-block'>7 years</strong> at <strong className='text-color-orange'>Tolq.com</strong>
+      <img src="/me.png" alt="That's me!" className={me}/>
+      <h1 className={hi}>Hi!</h1> 
+      <h1 className={intro}>        
+        I'm <strong>Rafa</strong> and I have spent the last
         {" "}
-        <strong className='text-color-green'>designing</strong> and <strong className='text-color-soft-blue'>implementing</strong> its 3 applications.        
-      </h1>
+        <strong className='inline-block'>8 years</strong> at <a href="https://tolq.com/">Tolq.com</a>
+        {" "}
+        <strong className="text-color-soft-blue">designing</strong> and <strong className="text-color-green">implementing</strong> its 
+        <br />
+        3 applications.        
 
-      <h1 className="align-center m-3 mt-5 mb-5">This is my portfolio:</h1>
+        <br />
+        <br />
+        This is a quick portfolio featuring a few of the things I have done:
+      </h1>      
     </div>
   );
 }
